d13f63172cc3d3457d8ee5345b5a9dc8bd6935b7
zurson <matu.jurek@gmail.com>
2023-04-20 11:38:52 +0200

    1. Wygenerowanie stalych i refaktoryzacja obecnych zmiennych na stale jesli to mozliwe

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int ARRAY_CAPACITY = 12;
+    public static final int TOTAL_NOT_CHANGED = -1;
+    public static final int ERROR_NUMBER = -1;
+    public static final int TOTAL_START_VALUE = -1;
+    private final int[] NUMBERS = new int[ARRAY_CAPACITY];
 
-    public int total = -1;
+    public int total = TOTAL_START_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class defaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_NOT_CHANGED;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_CAPACITY - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_NUMBER;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_NUMBER;
         return NUMBERS[total--];
     }
 
